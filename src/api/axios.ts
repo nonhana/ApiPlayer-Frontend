d440865ee6c0c877c2c6703b18b03c8affa00a59
Zhou Xiang <1209220829@qq.com>
2023-09-04 16:10:37 +0800

    小修改

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -3,7 +3,7 @@ import { ElNotification } from 'element-plus';
 
 const myAxios = (axiosConfig: AxiosRequestConfig): Promise<AxiosResponse> => {
 	const service = axios.create({
-// 		baseURL: 'http://127.0.0.1:3000', // 本地服务器环境(ApiPlayer-Backend)
+		// baseURL: 'http://127.0.0.1:3000', // 本地服务器环境(ApiPlayer-Backend)
 		baseURL: 'http://13.115.119.139:3000', // 正式环境(AWS-EC2-Server)
 		// baseURL: 'https://mock.apifox.cn/m1/3099285-0-default', // apifox云端mock环境
 		timeout: 100000, // 10秒内无响应则报错
