b79835bd95ec9a52399aa08cd6c8eb301d50e53a
Zhou Xiang <1209220829@qq.com>
2023-09-09 11:50:57 +0800

    更改proxy配置

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -4,8 +4,8 @@ import { ElNotification } from 'element-plus';
 const myAxios = (axiosConfig: AxiosRequestConfig): Promise<AxiosResponse> => {
 	const service = axios.create({
 		// baseURL: 'http://127.0.0.1:3000', // 本地服务器环境(ApiPlayer-Backend)
-		// baseURL: 'http://13.115.119.139:3000', // 正式环境(AWS-EC2-Server)
-		baseURL: '/baseURL', // proxy代理
+		baseURL: 'https://13.115.119.139:3000', // 正式环境(AWS-EC2-Server)
+		// baseURL: '/baseURL', // proxy代理
 		// baseURL: 'https://mock.apifox.cn/m1/3099285-0-default', // apifox云端mock环境
 		timeout: 1000000, // 1000秒内无响应则报错
 	});
